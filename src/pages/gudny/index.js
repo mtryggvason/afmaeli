70e9feaac7fbe0d806a4769565535ed160ba05b4
mtryggvason <90194562+golottie-magnus@users.noreply.github.com>
2024-05-27 22:54:13 +0200

    update guðny

--- src/pages/gudny/index.js
+++ src/pages/gudny/index.js
@@ -2,9 +2,7 @@
 import Slider from "react-slick";
 import Style from './gudny.module.scss';
 import Lottie from "lottie-react";
-import Gift from './gift.json';
 import { useRef } from "react";
-import { useState } from "react";
 import Confetti from 'react-confetti'
 import { useWindowSize } from "@react-hook/window-size";
 import { useEffect } from "react";
@@ -12,81 +10,56 @@ import { useEffect } from "react";
 const Slide1 = ({nextSlide}) => {
     return (
     <div onClick={() => nextSlide()} className={Style.wrapper}>
-      <img alt="flottust" className={Style.image} src='./gudny/gudny1.jpg' />
-      <p className={Style.h1}>Til hamingju með afmælið Guðny</p>
+      <img alt="flottust" className={Style.image} src='./gudny/baby.png' />
+      <p className={Style.h1}>Til hamingju með afmælið elsku Guðny</p>
     </div>);
 };
 
 
 const Slide2 = ({nextSlide}) => {
     return (<div className={Style.wrapper}>
-    <img alt="flottust" className={Style.image} src='./gudny/gudny2.jpg' />
-      <p className={Style.h1}>Við erum fáranlega ánægðir með þig!</p>
+    <img alt="flottust" className={Style.image} src='./gudny/koss.png' />
+      <p className={Style.h1}>Hér er stafrænn afmæliskoss frá okkur í Vín</p>
     </div>);
 };
-
 
 
 const Slide3 = ({nextSlide}) => {
-    return (<div className={Style.wrapper}>
-      <p className={Style.h1}>Til að fagna deginum þínum langar okkur að kaupa, handa þér....</p>
-        <img  className={Style.gift} src="./gudny/gift_closed.png"></img>
-    </div>);
+  return (<div className={Style.wrapper}>
+  <img alt="flottust" className={Style.image} src='./gudny/fjoll.png' />
+    <p className={Style.h1}>Okkur finnst þú frekar flott á fjöllum!</p>
+  </div>);
 };
-
 
 const Slide4 = ({nextSlide}) => {
   return (<div className={Style.wrapper}>
-    <p className={Style.h1}>Garn! </p>
-    <img className={Style.yarn} src="./gudny/yarn.png"></img>
-    <img className={`${Style.gift_open}`} src="./gudny/gift_open.png"></img>
+  <img alt="flottust" className={Style.image} src='./gudny/ludi.png' />
+    <p className={Style.h1}>Og líka bara yfir höfuð<small>(samt smá lúði á þessari mynd)!</small></p>
   </div>);
 };
 
 const Slide5 = ({nextSlide}) => {
-  return (<div className={Style.wrapper}>
-    <p className={Style.h1}>og.... </p>
-    <img className={Style.gift} src="./gudny/gift_closed.png"></img>
-  </div>);
+    return (<div className={Style.wrapper}>
+      <img  className={Style.image} src="./gudny/vin.jpeg"></img>
+      <p className={Style.h1}>Til að fagna deginum þínum langar okkur að splæsa nokkrar unmferðir á barinn.</p>
+    </div>);
 };
 
 
 const Slide6 = ({nextSlide}) => {
   return (<div className={Style.wrapper}>
-    <p className={Style.h1}>Miriam</p>
-    <img className={Style.miriam} src="./gudny/madMiri.png"></img>
-    <img className={`${Style.gift_open}`} src="./gudny/gift_open.png"></img>
+          <img className={Style.image} src="./gudny/vin.jpeg"></img>
+
+    <p className={Style.h1}>Þetta ætti að koma til þín í kvöld ef ég skil það rétt.</p>
   </div>);
 };
+
 
 const Slide7 = ({nextSlide}) => {
   return (<div className={Style.wrapper}>
-    <p className={Style.h1}>Sem vonandi verður að peysu í haust!</p>
-    <img className={Style.miriam} src="./gudny/sweater.png"></img>
-    <img className={`${Style.gift_open}`} src="./gudny/gift_open.png"></img>
-  </div>);
-};
-
-const Slide8 = ({nextSlide}) => {
-  return (
-  <div onClick={() => nextSlide()} className={Style.wrapper}>
-    <img alt="flottust" className={Style.image} src='./gudny/saman2.jpg' />
-    <p className={Style.h1}>Við hlökkum til í fleirri gæða stunda á hlaupum </p>
-  </div>);
-};
-
-
-const Slide9 = ({nextSlide}) => {
-  return (<div className={Style.wrapper}>
-  <img alt="flottust" className={Style.image} src='./gudny/saman.jpg' />
-    <p className={Style.h1}>eða á fjöllum!</p>
-  </div>);
-};
-
-const Slide10 = ({nextSlide}) => {
-  return (<div className={Style.wrapper}>
   <img alt="flottust" className={Style.image} src='./gudny/gudny3.jpg' />
-    <p className={Style.h1}>Kalli, Magnus og co!</p>
+    <p className={Style.h1}>Hlökkum til að hitta þig á Ísland</p>
+    <p> Magnus og co!</p>
   </div>);
 };
 
@@ -128,12 +101,6 @@ export default function() {
             <Slide6 nextSlide={() => {
             }} />
             <Slide7 nextSlide={() => {
-            }} />
-            <Slide8 nextSlide={() => {
-            }} />
-            <Slide9 nextSlide={() => {
-            }} />
-            <Slide10 nextSlide={() => {
             }} />
           </Slider>
         </div>
